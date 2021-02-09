3ede0c353420d2591f83613a662475fe83201c3b
yuto horioka <zxxp1341@gmail.com>
2021-02-09 17:02:58 +0900

    validate

--- main/adapter.py
+++ main/adapter.py
@@ -2,7 +2,17 @@ from django.contrib.sites.shortcuts import get_current_site
 from allauth.account.adapter import DefaultAccountAdapter
 from django.conf import settings
 
+
 class CustomAllauthAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         current_site = get_current_site(request)
         return '{}/confirm-email/?{}'.format(current_site, emailconfirmation.key)
+
+    def save_user(self, request, user, form, commit=True):
+        user = super(CustomAllauthAdapter, self).save_user(
+            request, user, form, commit=False)
+        email = request.POST['email']
+        email = email.split('@', 1)[1]
+        if(email != 'st.kyoto-u.ac.jp'):
+            return
+        user.save()
