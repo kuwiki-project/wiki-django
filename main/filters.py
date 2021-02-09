2ccbd3e0005804a728fac14f7baa6bd46cd7526e
yuto horioka <zxxp1341@gmail.com>
2021-02-09 16:00:55 +0900

    idfilter

--- main/filters.py
+++ main/filters.py
@@ -3,17 +3,22 @@ from .models import Course
 
 
 class CourseFilter(filters.FilterSet):
+    id = filters.NumberFilter(field_name='id', lookup_expr='exact')
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
     field = filters.CharFilter(field_name='field', lookup_expr='icontains')
-    year = filters.NumberFilter(field_name='lecture__year', lookup_expr='exact')
+    year = filters.NumberFilter(
+        field_name='lecture__year', lookup_expr='exact')
     instructor = filters.CharFilter(
         field_name='lecture__instructor__instructor', lookup_expr='icontains')
     period = filters.CharFilter(
         field_name='lecture__period__period', lookup_expr='icontains')
-    semester = filters.CharFilter(field_name='semester', lookup_expr='icontains')
+    semester = filters.CharFilter(
+        field_name='semester', lookup_expr='icontains')
     major = filters.CharFilter(field_name='major', lookup_expr='icontains')
-    exam_count = filters.NumberFilter(field_name='exam_count', lookup_expr='gt')
+    exam_count = filters.NumberFilter(
+        field_name='exam_count', lookup_expr='gt')
 
     class Meta:
         model = Course
-        fields = ['name', 'field', 'year', 'instructor', 'period', 'semester', 'major', 'exam_count']
+        fields = ['id', 'name', 'field', 'year', 'instructor',
+                  'period', 'semester', 'major', 'exam_count']
