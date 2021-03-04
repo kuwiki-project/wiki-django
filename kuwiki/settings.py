a519388e35e9faeb1723e4bd08ea041362b88dc5
kuwiki <kuwiki99@gmail.com>
2021-03-04 09:19:21 +0900

    collectstatic

--- kuwiki/settings.py
+++ kuwiki/settings.py
@@ -146,7 +146,7 @@ SITE_ID = 1
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
