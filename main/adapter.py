5ef8f8d63343d60875422078fa36eefeb093141c
kuwiki <60818535+kuwiki@users.noreply.github.com>
2021-07-05 06:55:12 +0900

    Update auth url
    wiki-nuxt，アカウント認証系のURLの前に/auth/といれることにした

--- main/adapter.py
+++ main/adapter.py
@@ -6,7 +6,7 @@ from django.conf import settings
 class CustomAllauthAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         current_site = get_current_site(request)
-        return '{}/confirm-email/?key={}'.format(current_site, emailconfirmation.key)
+        return '{}/auth/confirm-email/?key={}'.format(current_site, emailconfirmation.key)
 
     def save_user(self, request, user, form, commit=True):
         user = super(CustomAllauthAdapter, self).save_user(request, user, form, commit=False)
