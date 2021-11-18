49a1b385568cf984396bdb8e3d79bc9eeeed53e6
Y.H <36223382+Copynacht@users.noreply.github.com>
2021-11-18 18:30:20 +0900

    change locale locale

--- kuwiki/settings.py
+++ kuwiki/settings.py
@@ -63,13 +63,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
     'corsheaders.middleware.CorsMiddleware',
 ]
 
